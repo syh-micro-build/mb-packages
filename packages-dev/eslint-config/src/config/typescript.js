5495742447734e599d095116c75b429e3bf5b06e
李勇 <yong.li@yidatec.com>
2025-01-10 17:14:41 +0800

    feat: ts eslint

--- packages-dev/eslint-config/src/config/typescript.js
+++ packages-dev/eslint-config/src/config/typescript.js
@@ -13,7 +13,7 @@ export default {
       ecmaVersion: "latest",
       extraFileExtensions: [".vue"],
       jsxPragma: "React",
-      project: "./tsconfig.*?.json",
+      project: "**/tsconfig.*?.json",
       sourceType: "module"
     }
   },
@@ -31,12 +31,10 @@ export default {
         "ts-ignore": "allow-with-description",
         "ts-nocheck": "allow-with-description"
       }
-    ],
+    ], // 禁止使用 // @ts-expect-error 和 // @ts-ignore 注释
     "@typescript-eslint/ban-ts-ignore": "off", // 禁止使用 // @ts-ignore 注释
-    // "@typescript-eslint/ban-ts-comment": "off", // 禁止使用 // @ts-expect-error 和 // @ts-ignore 注释
     "@typescript-eslint/ban-types": "off", // 禁止使用特定类型
     "@typescript-eslint/consistent-type-definitions": ["warn", "interface"],
-    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/naming-convention": [
       "error",
       {
@@ -49,10 +47,12 @@ export default {
           match: false,
           regex: "[A-Z\\d]__[A-Z\\d]"
         },
-        filter: {
-          match: true,
-          regex: "_[A-Z\\d]"
-        },
+
+        // filter: {
+        //   match: true,
+        //   regex: "_[A-Z\\d]"
+        // },
+
         format: ["strictCamelCase", "StrictPascalCase", "UPPER_CASE"],
         selector: "variable"
       },
@@ -97,12 +97,13 @@ export default {
       },
       {
 
-        // Allow anything in destructured properties
+        // 允许解构属性使用任何命名
         format: null,
         modifiers: ["destructured"],
         selector: ["variable", "parameter"]
       }
     ],
+
     "@typescript-eslint/no-duplicate-enum-values": "error", // 不允许重复的枚举
     "@typescript-eslint/no-empty-function": [
       "error",
@@ -118,31 +119,18 @@ export default {
 
     "@typescript-eslint/no-require-imports": "off",
 
-    // "@typescript-eslint/explicit-module-boundary-types": "off", // 要求导出函数和方法的返回类型必须显式声明
+    "@typescript-eslint/explicit-module-boundary-types": "off", // 要求导出函数和方法的返回类型必须显式声明
     "@typescript-eslint/no-undef": "off",
 
     "@typescript-eslint/no-unsafe-function-type": "warn",
 
     "@typescript-eslint/no-unused-expressions": "off",
 
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_"
-      }
-    ],
-
-    // "@typescript-eslint/no-empty-function": "off", // 禁止空函数
     "@typescript-eslint/no-use-before-define": "off", // 禁止在变量声明之前使用它们
-    "@typescript-eslint/no-var-requires": "error" // 禁止使用 require
+    "@typescript-eslint/no-var-requires": "error", // 禁止使用 require
 
     // "@typescript-eslint/no-non-null-assertion": "off", // 禁止使用非空断言操作符 !
-    /**
-     * Ts Diy
-    /**
-     * @link https://typescript-eslint.io/rules/no-unused-vars
-     */
+
     /*
      * '@typescript-eslint/no-unused-vars': [
      *   'error',
@@ -152,28 +140,28 @@ export default {
      *   },
      * ],
      */
-    // "@typescript-eslint/no-unused-vars": ["error", {
-    //   vars: "all",
-    //   args: "after-used",
-    //   ignoreRestSiblings: true
-    // }],
-    // '@typescript-eslint/indent': ['error', 2, {
-    //   SwitchCase: 1,
-    //   ArrayExpression: 1,
-    //   MemberExpression: 2,
-    //   CallExpression: {
-    //     arguments: 2
-    //   },
-    //   FunctionExpression: {
-    //     body: 1,
-    //     parameters: 2
-    //   },
-    //   FunctionDeclaration: {
-    //     body: 1,
-    //     parameters: 2
-    //   }
-    // }], // 设置缩进为两个空格
-    // '@typescript-eslint/type-annotation-spacing': ['error'],
-    // '@typescript-eslint/explicit-function-return-type': ['warn'] // 要求函数的返回类型必须显式声明
+    "@typescript-eslint/no-unused-vars": ["error", {
+      vars: "all",
+      args: "after-used",
+      ignoreRestSiblings: true
+    }],
+
+    "@typescript-eslint/indent": ["error", 2, {
+      SwitchCase: 1,
+      ArrayExpression: 1,
+      MemberExpression: 2,
+      CallExpression: {
+        arguments: 2
+      },
+      FunctionExpression: {
+        body: 1,
+        parameters: 2
+      },
+      FunctionDeclaration: {
+        body: 1,
+        parameters: 2
+      }
+    }], // 设置缩进为两个空格
+    "@typescript-eslint/explicit-function-return-type": ["warn"] // 要求函数的返回类型必须显式声明
   }
 };
