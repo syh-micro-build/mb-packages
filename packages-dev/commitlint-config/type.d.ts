d364ad4dad47901307c8e04e11bfebe9b5755bcd
李勇 <yong.li@yidatec.com>
2025-01-14 11:22:10 +0800

    fix: 处理 mb-commitlint-config 提示未安装类型文件

---
+++ packages-dev/commitlint-config/type.d.ts
@@ -0,0 +1,10 @@
+declare module "mb-commitlint-config" {
+
+  // 定义模块的导出类型
+  export interface IConfig {
+    rules: Record<string, unknown>;
+  }
+
+  const config: IConfig;
+  export default config;
+}
