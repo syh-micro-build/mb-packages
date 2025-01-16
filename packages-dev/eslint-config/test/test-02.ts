754541c23184b69a8bd48f874f107bcce06f2e40
李勇 <yong.li@yidatec.com>
2025-01-16 19:43:37 +0800

    docs: todo json style no the same

--- packages-dev/eslint-config/test/test-02.ts
+++ packages-dev/eslint-config/test/test-02.ts
@@ -13,3 +13,13 @@ export const obj = {
   a: 1,
   b: 2
 };
+
+// 测试数据
+export const data = {
+  label: "John Doe",
+  "type": "EUiEleType.BORDER",
+  "options": {
+    "street": "123 Main St",
+    "city": "Somewhere"
+  }
+};
