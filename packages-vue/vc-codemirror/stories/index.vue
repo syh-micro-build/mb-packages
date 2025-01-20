114953ae66d663f9fda3ffe759ee2f8c3a71a55a
李勇 <yong.li@yidatec.com>
2025-01-20 17:23:56 +0800

    feat: mb-vc-codemirror

--- packages-vue/vc-codemirror/stories/index.vue
+++ packages-vue/vc-codemirror/stories/index.vue
@@ -3,19 +3,31 @@ import Codemirror, {
   CodeMirrorType
 } from "../src";
 
-const str = "{\"name\":\"John Doe\",\"age\":30,\"isDeveloper\":true,\"languages\":[\"JavaScript\",\"TypeScript\",\"Python\"],\"address\":{\"street\":\"123 Main St\",\"city\":\"New York\",\"country\":\"USA\"}}";
+const str1 = `
+{
+  type: "Button",
+  label: "你好",
+}
+`;
+
+const str2 = "<Button>你好</Button>";
 </script>
 
 <template>
   <Codemirror
-    :value="str"
+    :value="str1"
     :type="CodeMirrorType.JSON"
   />
+  <Codemirror
+    :value="str2"
+    :type="CodeMirrorType.VUE"
+  />
+  <Button>你好</Button>
 </template>
 
 <style scoped>
 div {
-  height: 900px;
+  height: 200px;
 }
 
 </style>
--- packages-vue/vc-codemirror/vite.config.ts
+++ packages-vue/vc-codemirror/vite.config.ts
@@ -4,11 +4,48 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import {
   defineConfig
 } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx()
-  ]
+    vueJsx(),
+    dts({
+      outDir: "./dist/types",
+      tsconfigPath: "./tsconfig.json",
+      rollupTypes: true
+    })
+  ],
+  build: {
+    target: "esnext",
+    minify: true,
+    sourcemap: false,
+    rollupOptions: {
+      external: ["vue"],
+      input: "./src/index.ts",
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          preserveModules: true,
+          dir: "dist/es",
+          preserveModulesRoot: "src"
+        },
+        {
+          format: "umd",
+          name: "microVueHooks",
+          entryFileNames: "index.js",
+          dir: "dist/umd",
+          globals: {
+            vue: "Vue"
+          }
+        }
+      ]
+    },
+    lib: {
+      entry: "./src/index.ts",
+      formats: ["es", "umd"]
+    }
+  }
 });
