b80776e6af47784b37a7da8534dac5c921b999f9
李勇 <yong.li@yidatec.com>
2025-01-20 11:40:43 +0800

    feat: element-x

---
+++ packages-build-components-vue/element-x/src/const/component-map.ts
@@ -0,0 +1,9 @@
+import {
+  ElButton
+} from "element-plus";
+
+const COMPONENT_MAP = {
+  Button: ElButton
+};
+
+export default COMPONENT_MAP;
---
+++ packages-build-components-vue/element-x/stories/index.vue
@@ -0,0 +1,13 @@
+<script lang="tsx" setup>
+import {
+  COMPONENT_MAP
+} from "../src";
+</script>
+
+<template>
+  <COMPONENT_MAP.Button>222</COMPONENT_MAP.Button>
+</template>
+
+<style scoped>
+
+</style>
