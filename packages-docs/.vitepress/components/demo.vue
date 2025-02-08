0191dd89d0313bf584180f269bab9dfbc3ef8859
李勇 <yong.li@yidatec.com>
2025-02-08 20:10:34 +0800

    feat: 删除之前错误展示

--- packages-docs/.vitepress/components/demo.vue
+++ packages-docs/.vitepress/components/demo.vue
@@ -51,24 +51,6 @@ const idState = inject<Ref<ConfigProviderProps>>(CONFIG_PROVIDER)!;
       </div>
     </div>
   </div>
-
-  <!--
- <div class="demo-block">
-    <div class="demo-content">
-      <slot name="source"></slot>
-    </div>
-    <div
-      v-if="decodedDescription"
-      class="demo-description"
-      v-html="decodedDescription"
-    ></div>
-    <div
-      v-if="decodedSource"
-      class="demo-source"
-      v-html="decodedSource"
-    ></div>
-  </div>
--->
 </template>
 
 <style scoped>
@@ -80,25 +62,4 @@ div {
   border: 1px solid #DCDFE7;
   border-radius: 4px;
 }
-
-/* .demo-block {
-  margin: 1rem 0;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-}
-
-.demo-content {
-  padding: 1.5rem;
-}
-
-.demo-description {
-  padding: 1rem;
-  background-color: #f9f9f9;
-  border-top: 1px solid #ddd;
-}
-
-.demo-source {
-  padding: 1rem;
-  border-top: 1px solid #ddd;
-} */
 </style>
