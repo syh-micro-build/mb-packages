ca8a50cf243963f537aa497b5b655fce2a8b7cc9
李勇 <yong.li@yidatec.com>
2025-02-18 20:06:38 +0800

    feat: 实现思路

--- packages-components-vue/container/plugins/component-map-plugin.ts
+++ packages-components-vue/container/plugins/component-map-plugin.ts
@@ -35,6 +35,16 @@ export default function componentMapPlugin(): Plugin {
           ${exportStatement}
         `;
       }
+    },
+
+    /**
+     * 在热更新时，生成一个配置文件，然后读取配置文件
+     *
+     * 在 provide 处生成配置文件，在这进行读取，然后把 load 里面的内容，想办法，迁移过来
+     */
+    handleHotUpdate() {
+
+      // console.log(222);
     }
   };
 }
