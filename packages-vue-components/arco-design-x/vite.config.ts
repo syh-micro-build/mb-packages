875bf855067187874a8338d71584d6139d432b4c
李勇 <yong.li@yidatec.com>
2025-02-20 10:45:18 +0800

    feat: arco-design-x

--- packages-vue-components/arco-design-x/vite.config.ts
+++ packages-vue-components/arco-design-x/vite.config.ts
@@ -4,11 +4,13 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import {
   defineConfig
 } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    cssInjectedByJsPlugin(),
     vue(),
     vueJsx(),
     dts({
@@ -20,24 +22,24 @@ export default defineConfig({
   build: {
     target: "esnext",
     minify: true,
-    sourcemap: false,
+    sourcemap: true,
+    cssCodeSplit: false,  // 禁用 CSS 拆分，CSS 会被内联到 JS 文件
     rollupOptions: {
-      external: ["vue"],
-      input: "./src/index.ts",
+      external: ["vue"],  // Vue 作为外部依赖
+      input: "./src/index.ts", // 入口文件
       output: [
         {
-          format: "es",
+          format: "es",  // 使用 ES 模块格式
           entryFileNames: "[name].js",
           preserveModules: true,
           dir: "dist",
-          preserveModulesRoot: "src",
-          assetFileNames: "index.css"
+          preserveModulesRoot: "src"
         }
       ]
     },
     lib: {
-      entry: "./src/index.ts",
-      formats: ["es"]
+      entry: "./src/index.ts",  // 入口文件
+      formats: ["es"]  // 输出 ES 模块格式
     }
   }
 });
