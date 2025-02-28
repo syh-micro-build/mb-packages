467973dc0b1eab08f7caff82bb91c36c977fa005
李勇 <yong.li@yidatec.com>
2025-02-28 18:03:33 +0800

    feat: Success show

--- packages-vue-components/check-json-schema/stories/index.stories.ts
+++ packages-vue-components/check-json-schema/stories/index.stories.ts
@@ -1,16 +1,53 @@
 import Index from "./index.vue";
+import Success from "./success.vue";
 import type {
-  Meta,
-  StoryObj
+  Meta, StoryObj
 } from "@storybook/vue3";
 
 const meta = {
   component: Index,
-  title: "Demo"
+  title: "Demo",
+  argTypes: {
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    isSuccess: {
+      control: "boolean"
+    }
+  }
 } satisfies Meta<typeof Index>;
 
 type TStory = StoryObj<typeof meta>;
 
-export const Demo: TStory = {};
+const Template: TStory["render"] = args => ({
+  components: {
+    Index,
+    Success
+  },
+  setup: () => ({
+    args
+  }),
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  template: args.isSuccess ? "<Success v-bind=\"args\" />" : "<Index v-bind=\"args\" />"
+});
+
+export const Demo: TStory = {
+  render: Template,
+  args: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    isSuccess: false
+  }
+};
+
+export const SuccessState: TStory = {
+  render: Template,
+  args: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    isSuccess: true
+  }
+};
 
 export default meta;
